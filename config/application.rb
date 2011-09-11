d96d11740123e40a932afb9dcd2a415a59991d19
Paul A. Jungwirth <pj@illuminatedcomputing.com>
2011-09-11 13:05:41 -0400

    Lots of work on the infrastructure

--- config/application.rb
+++ config/application.rb
@@ -38,5 +38,11 @@ module PennappsJquery
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
+
+    config.action_view.javascript_expansions[:defaults] = [
+      'jquery.min',
+      'jquery-ui.min'
+      # 'application'
+    ]
   end
 end
